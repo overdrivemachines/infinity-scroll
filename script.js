ff53650fbc4e1bbf873e73e1c57090fa5c366738
get.dipen@gmail.com <get.dipen@gmail.com>
2022-08-15 02:46:36 -0700

    Automatically load new images when user has scrolled to bottom of page

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'fqL0CaHVUzQJIizi6yIgKW7qtVD4NMCEwN6rqiAa7DA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  console.log('image loaded');
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    console.log('ready =', ready);
+  }
+}
+
 
 // Helper Function to SetAttributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -18,6 +32,9 @@ function setAttributes(element, attributes) {
 
 // Create Elements For Links & Photos,Add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length
+  console.log('total images', totalImages)
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Upslash
@@ -38,6 +55,9 @@ function displayPhotos() {
         title: photo.alt_description
       });
     }
+
+    // Event Listner, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
 
     // Put <img> inside <a>, then put both inside .imageContainer element
     item.appendChild(img);
@@ -62,7 +82,8 @@ async function getPhotos() {
 window.addEventListener('scroll', () => {
   // console.log(window.scrollY);
 
-  if (window.innerHeight + window.scrollY > document.body.offsetHeight - 1000) {
+  if ((window.innerHeight + window.scrollY > document.body.offsetHeight - 1000) && ready) {
+    ready = false;
     getPhotos();
   }
 });
